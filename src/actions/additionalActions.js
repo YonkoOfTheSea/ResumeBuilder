7121bd163d6626a712ad65a43038cacb41b56fb8
Aman <71187941+YonkoOfTheSea@users.noreply.github.com>
2021-09-15 23:42:22 +0530

    initial

--- src/actions/additionalActions.js
+++
@@ -1,7 +0,0 @@
-import * as actionTypes from './actionTypes';
-export const add=(documentId, additionalSection)=>{
-    return {type: actionTypes.ADD_ADDITIONAL, additionalSection:additionalSection}
-}
-export const update=(documentId, additionalSection)=>{
-    return {type: actionTypes.UPDATE_ADDITIONAL, additionalSection:additionalSection} 
-}
--- src/actions/projectsActions.js
+++
@@ -1,7 +0,0 @@
-import * as actionTypes from './actionTypes';
-export const add=(documentId, projectsSection)=>{
-    return {type: actionTypes.ADD_PROJECTS, projectsSection:projectsSection}
-}
-export const update=(documentId, projectsSection)=>{
-    return {type: actionTypes.UPDATE_PROJECTS, projectsSection:projectsSection} 
-}
